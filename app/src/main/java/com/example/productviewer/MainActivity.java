42d6ef0b28d4880f6a4b35eced9cfdbd673653f5
Degtyarev111 <degtyarevmishas@yandex.ru>
2024-03-10 23:12:06 +0300

    Product GUI update

--- app/src/main/java/com/example/productviewer/MainActivity.java
+++ app/src/main/java/com/example/productviewer/MainActivity.java
@@ -4,11 +4,7 @@ package com.example.productviewer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
